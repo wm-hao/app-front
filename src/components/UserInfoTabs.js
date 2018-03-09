ae7af9fd03449db92ae306eba2bf880e8c10830c
v_zhh1026 <448826602@qq.com>
2018-03-09 18:07:19 +0800

    go go

--- src/components/UserInfoTabs.js
+++ src/components/UserInfoTabs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Tabs} from 'antd';
 import UserInfoForm from "./UserInfoForm";
+import UserInfoUpdate2 from "./UserInfoUpdate2";
 
 const TabPane = Tabs.TabPane;
 class UserInfoTabs extends React.Component {
@@ -9,7 +10,8 @@ class UserInfoTabs extends React.Component {
         return(
             <div>
                 <Tabs tabPosition={'top'}>
-                    <TabPane tab="个人信息" key="1"><UserInfoForm userInfo={this.props.userInfo}/></TabPane>
+                    {/*<TabPane tab="个人信息" key="1"><UserInfoForm userInfo={this.props.userInfo}/></TabPane>*/}
+                    <TabPane tab="个人信息" key="1"><UserInfoUpdate2 userInfo={this.props.userInfo}/></TabPane>
                     <TabPane tab="修改密码" key="2">密码修改</TabPane>
                 </Tabs>
             </div>
